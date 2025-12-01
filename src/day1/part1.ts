0d7e3d3b6f7c86094c8a5588970c52f25e3207c0
Amani Kilumanga <amani.kilumanga@gmail.com>
2025-12-01 12:51:13 +0100

    feat: solved day 1 part 2; split solution scripts

---
+++ src/day1/part1.ts
@@ -0,0 +1,31 @@
+import { readFileSync } from 'node:fs'
+import { decrementDial, incrementDial } from '..'
+
+
+
+export const solve = () => {
+    const input = readFileSync("input.txt", "utf8")
+
+    let current = 50
+    let zeroCount = 0
+
+    for (const row of input.split("\n")) {
+        const rotationDirection = row.substring(0, 1)
+        const rotationSteps = parseInt(row.substring(1), 10)
+
+        for (let i = 0; i < rotationSteps; i++) {
+            if (rotationDirection === "L") {
+                current = decrementDial(current)
+            } else {
+                current = incrementDial(current)
+            }
+        }
+
+        console.log(`the dial was rotated ${rotationDirection}${rotationSteps} to point at ${current}`)
+        if (current === 0) {
+            zeroCount++
+        }
+    }
+
+    console.log(`the dial pointed at 0 ${zeroCount} times`)
+}
---
+++ src/day1/part2.ts
@@ -0,0 +1,32 @@
+import { readFileSync } from 'node:fs'
+import { decrementDial, incrementDial } from '..'
+
+export const solve = () => {
+    const input = readFileSync("input.txt", "utf8")
+
+    let current = 50
+    let zeroCount = 0
+
+    for (const row of input.split("\n")) {
+        let rowZeroCount = 0
+
+        const rotationDirection = row.substring(0, 1)
+        const rotationSteps = parseInt(row.substring(1), 10)
+
+        for (let i = 0; i < rotationSteps; i++) {
+            if (rotationDirection === "L") {
+                current = decrementDial(current)
+            } else {
+                current = incrementDial(current)
+            }
+            if (current === 0) {
+                rowZeroCount++
+            }
+        }
+
+        console.log(`the dial was rotated ${rotationDirection}${rotationSteps} to point at ${current}; it pointed at 0 ${rowZeroCount} times`)
+        zeroCount += rowZeroCount
+    }
+
+    console.log(`the dial pointed at 0 ${zeroCount} times`)
+}
