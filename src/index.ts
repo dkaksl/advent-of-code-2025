522caadc56fb74a89c6fdcceb19536e66a9822c2
Amani Kilumanga <amani.kilumanga@gmail.com>
2025-12-01 12:51:34 +0100

    feat: added selection of script by args

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,27 @@
-import { readFileSync } from 'node:fs'
 
+import { solve as solveDay1Part1 } from './day1/part1'
+import { solve as solveDay1Part2 } from './day1/part2'
 const main = () => {
-    const input = readFileSync("input.txt", "utf8")
-
-    let current = 50
-    let zeroCount = 0
-
-    for (const row of input.split("\n")) {
-        const rotationDirection = row.substring(0, 1)
-        const rotationSteps = parseInt(row.substring(1), 10)
-
-        for (let i = 0; i < rotationSteps; i++) {
-            if (rotationDirection === "L") {
-                current = decrementDial(current)
-            } else {
-                current = incrementDial(current)
-            }
-        }
-
-        console.log(`the dial was rotated ${rotationDirection}${rotationSteps} to point at ${current}`)
-        if (current === 0) {
-            zeroCount++
-        }
+    const args = process.argv.slice(2)
+    if (args.length !== 2) {
+        throw new Error(`invalid number of args: ${args.length}`)
     }
 
-    console.log(`the dial pointed at 0 ${zeroCount} times`)
+    const day = parseInt(args[0], 10)
+    const part = parseInt(args[1], 10)
+
+    if (day === 1) {
+        if (part === 1) {
+            solveDay1Part1()
+        }
+        else if (part === 2) {
+            solveDay1Part2()
+        }
+
+    }
 }
 
-const incrementDial = (current: number) => {
+export const incrementDial = (current: number) => {
     if (current === 99) {
         return 0
     }
@@ -35,7 +29,7 @@ const incrementDial = (current: number) => {
     return current + 1
 }
 
-const decrementDial = (current: number) => {
+export const decrementDial = (current: number) => {
     if (current === 0) {
         return 99
     }
