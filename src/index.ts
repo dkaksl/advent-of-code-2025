c68c417bbefe70aa62e85fa7fdef9cf4fe2fcda0
Amani Kilumanga <amani.kilumanga@gmail.com>
2025-12-02 09:01:19 +0100

    chore: added route to solve day 2 challenges

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 
 import { solve as solveDay1Part1 } from './day1/part1'
 import { solve as solveDay1Part2 } from './day1/part2'
+import { solve as solveDay2Part1 } from './day2/part1'
 const main = () => {
     const args = process.argv.slice(2)
     if (args.length !== 2) {
@@ -13,11 +14,14 @@ const main = () => {
     if (day === 1) {
         if (part === 1) {
             solveDay1Part1()
-        }
-        else if (part === 2) {
+        } else if (part === 2) {
             solveDay1Part2()
         }
 
+    } else if (day === 2) {
+        if (part === 1) {
+            solveDay2Part1()
+        }
     }
 }
 
