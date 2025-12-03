113b2b08433d2a605667a13b7ba6c83a405a8163
Amani Kilumanga <amani.kilumanga@gmail.com>
2025-12-03 09:54:16 +0100

    chore: dynamically pick solve function

--- typescript-node/src/index.ts
+++ typescript-node/src/index.ts
@@ -1,8 +1,4 @@
 
-import { solve as solveDay1Part1 } from './day1/part1'
-import { solve as solveDay1Part2 } from './day1/part2'
-import { solve as solveDay2Part1 } from './day2/part1'
-import { solve as solveDay2Part2 } from './day2/part2'
 const main = () => {
     const args = process.argv.slice(2)
     if (args.length !== 2) {
@@ -12,37 +8,7 @@ const main = () => {
     const day = parseInt(args[0], 10)
     const part = parseInt(args[1], 10)
 
-    if (day === 1) {
-        if (part === 1) {
-            solveDay1Part1()
-        } else if (part === 2) {
-            solveDay1Part2()
-        }
-
-    } else if (day === 2) {
-        if (part === 1) {
-            solveDay2Part1()
-        }
-        else if (part === 2) {
-            solveDay2Part2()
-        }
-    }
-}
-
-export const incrementDial = (current: number) => {
-    if (current === 99) {
-        return 0
-    }
-
-    return current + 1
-}
-
-export const decrementDial = (current: number) => {
-    if (current === 0) {
-        return 99
-    }
-
-    return current - 1
+    require(`./day${day}/part${part}`).solve()
 }
 
 main()
