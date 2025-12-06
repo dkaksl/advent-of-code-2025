4b9847fe26bfe24bca47ce8d68627299bb191ce9
Amani Kilumanga <amani.kilumanga@gmail.com>
2025-12-06 11:15:02 +0100

    chore: formatting

--- typescript-node/src/index.ts
+++ typescript-node/src/index.ts
@@ -1,16 +1,15 @@
-
 const main = () => {
-    const args = process.argv.slice(2)
-    if (args.length !== 2) {
-        throw new Error(`invalid number of args: ${args.length}`)
-    }
+  const args = process.argv.slice(2)
+  if (args.length !== 2) {
+    throw new Error(`invalid number of args: ${args.length}`)
+  }
 
-    const day = parseInt(args[0], 10)
-    const part = parseInt(args[1], 10)
+  const day = parseInt(args[0], 10)
+  const part = parseInt(args[1], 10)
 
-    console.log(`solving day ${day} part ${part} challenge`)
+  console.log(`solving day ${day} part ${part} challenge`)
 
-    require(`./day${day}/part${part}`).solve()
+  require(`./day${day}/part${part}`).solve()
 }
 
 main()
